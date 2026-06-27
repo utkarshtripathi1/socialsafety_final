f3afa42f740c95d036b12b6ba6d42fed06d197b3
utkarshtipahi1 <utripathi@gmail.com>
2026-06-27 14:19:41 +0530

    utkarsh commit

--- socialsafety_1/urls.py
+++ socialsafety_1/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path 
 from socialsafety_1 import views
 urlpatterns = [
-    path("social/",views.fun_1),
+    path("Home/",views.Home),
+    path("login/",views.login),
+    path("contacts/",views.contact),
+    path("signup/",views.signup),
+    path("dashboard/",views.dashboard),
+    path("profile/",views.profile),
+    path("report/",views.report),
+    
+
 ]
--- socialsafety_1/views.py
+++ socialsafety_1/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def fun_1(request):
+def Home(request):
     return render(request , "Home.html")
+
+def login(request):
+    return render(request , "login.html")
+    
+def profile(request):
+    return render(request , "profile.html")
+
+def contact(request):
+    return render(request , "contacts.html")
+
+def dashboard(request):
+    return render(request , "dashboard.html")
+
+def signup(request):
+    return render(request , "signup.html")
+
+def report(request):
+    return render(request , "report.html")
